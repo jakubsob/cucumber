94d29a894805879f2abe3af017f9541f06c1c672
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-02-10 18:19:29 +0100

    ✨ add run step function

--- R/step.R
+++ R/step.R
@@ -1,13 +1,38 @@
-#' @importFrom rlang exec
-step <- function(description, implementation, .feature) {
-  self = attr(.feature, "self")
-  definition <- find_definition(.feature, description)
-  params <- extract_params(definition, description)
-  exec(implementation, !!!params, context = attr(.feature, "context"))
+#' Creates a regex from step name and a description
+#'
+#' Allows whitespace before and after the description
+#' @keywords internal
+#' @noRd
+step_regex <- function(step_name, description) {
+  paste0("^\\s*", step_name, " ", description, "\\s*$")
 }
 
-given <- step
+#' @keywords internal
+#' @importFrom rlang exec
+make_step <- function(prefix) {
+  function(description, implementation) {
+    structure(
+      list(
+        description = step_regex(prefix, description),
+        implementation = implementation
+      ),
+      class = "step"
+    )
+  }
+}
 
-when <- step
+given <- make_step("Given")
 
-then <- step
+when <- make_step("When")
+
+then <- make_step("Then")
+
+#' @importFrom rlang exec
+run.step <- function(step, input, context) {
+  params <- extract_params(input, step$description)
+  exec(
+    step$implementation,
+    !!!params,
+    context = context
+  )
+}
