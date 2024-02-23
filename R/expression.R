2ad3a4e9187704dcd6fbb5ddc50192782ee965ad
Jakub Sobolewski <jakub.sobolewski@appsilon.com>
2024-02-23 21:22:51 +0100

    ♻️ use get_parameter instead of getOption

--- R/expression.R
+++ R/expression.R
@@ -1,6 +1,6 @@
 #' @importFrom purrr set_names map_chr
 #' @importFrom stringr str_replace_all
-expression_to_pattern <- function(x, parameters = getOption("parameters")) {
+expression_to_pattern <- function(x, parameters = get_parameters()) {
   replace <- set_names(
     map_chr(parameters, \(x) paste0("(", x$regex, ")")),
     map_chr(parameters, \(x) paste0("\\{", x$name, "\\}"))
